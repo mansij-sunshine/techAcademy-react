45e96694beba72af943a91ed2a803ac2f31b9ff0
mansij88 <mansij88@gmail.com>
2022-11-18 18:53:27 +0100

    Initial files for the React version of Tech Academy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <section className='section'>
+    <div className='nav'>
+    <div className='nav logo'>
+    <strong>Tech Academy</strong>
+    <img src="/tech.png" alt="icon" />
+    </div>
+    <ul className='nav items'>
+      <li><a href='#'>Courses</a></li>
+      <li><a href='#'>Testimonials</a></li>
+      <li><a href="#">Sign In</a></li>
+      <li><a href="#">Login</a></li>
+    </ul>
+    </div>
+    <div className='container one'>
+    <video className='video' autoPlay muted loop>
+      <source src="video.mp4"></source>
+    </video>
+    <strong className='textonvid'>
+    <h1>Welcome to Tech Academy</h1>
+    <p>The best place to learn technology.</p>
+    </strong>
+     
+    </div>    
+     </section>
+     
+     <section>
+     <div className='container first'>
+     <strong className='conheading'>Live & Recorded Courses</strong>
+     <div className="coursesblock"></div>
+     </div>
+     </section>
+
+     <section>
+     <div className='container second'>
+     <strong className='conheading'>Book a counselling session with us</strong>
+     </div>
+     </section>
+
+
     </div>
   );
 }
